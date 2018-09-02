6b8e2a575a90f3de5eba90faf2cdd9437840ffdc
JPRaphael <joaopedroraphael97@hotmail.com>
2018-09-02 20:43:07 -0300

    config banco de dados heroku

--- src/main/java/com/jpraphael/ajubus/services/OnibusService.java
+++ src/main/java/com/jpraphael/ajubus/services/OnibusService.java
@@ -1,6 +1,5 @@
 package com.jpraphael.ajubus.services;
 
-import com.jpraphael.ajubus.model.Manutencao;
 import com.jpraphael.ajubus.model.Onibus;
 import com.jpraphael.ajubus.utils.GenericDao;
 
@@ -8,15 +7,6 @@ import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.Root;
-import javax.ws.rs.PathParam;
-import java.util.List;
 
 @Stateless
 @TransactionAttribute(TransactionAttributeType.SUPPORTS)
